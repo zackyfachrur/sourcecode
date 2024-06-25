5aeb0675bf77c01cae1464cde29c8f1f7cb0fdec
mzackyfa <116891033+zackyfachrur@users.noreply.github.com>
2024-06-25 20:09:54 +0700

    Delete domdasar directory

--- domdasar/js/main.js
+++
@@ -1,6 +0,0 @@
-const showAlert = document.getElementById('showAlert');
-const alertContent = document.getElementById('alertContent');
-
-showAlert.addEventListener('click', () => {
-   alertContent.classList.toggle('d-none')
-})
